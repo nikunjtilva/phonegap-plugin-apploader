cb0cc57ef32219b09eb7773b7e6068a086eae484
Nikunj <nikunjltilva@gmail.com>
2016-01-20 11:23:49 +0530

    Update AppLoader.java
    Removed 3 finger tap support.

--- src/android/src/com/phonegap/build/AppLoader.java
+++ src/android/src/com/phonegap/build/AppLoader.java
@@ -213,7 +213,7 @@ public class AppLoader extends CordovaPlugin {
                     @Override
                     public void onPageFinished(WebView view, String url) {
                         // Michael Brooks' homepage.js (https://github.com/phonegap/connect-phonegap/blob/master/res/middleware/homepage.js)
-                        String javascript="javascript: console.log('adding homepage.js'); (function(){var e={},t={touchstart:'touchstart',touchend:'touchend'};if(window.navigator.msPointerEnabled){t={touchstart:'MSPointerDown',touchend:'MSPointerUp'}}document.addEventListener(t.touchstart,function(t){var n=t.touches||[t],r;for(var i=0,s=n.length;i<s;i++){r=n[i];e[r.identifier||r.pointerId]=r}},false);document.addEventListener(t.touchend,function(t){var n=Object.keys(e).length;e={};if(n===3){t.preventDefault();window.history.back(window.history.length)}},false)})(window)";
+                        String javascript="javascript: console.log('adding homepage.js'); (function(){var e={},t={touchstart:'touchstart',touchend:'touchend'};if(window.navigator.msPointerEnabled){t={touchstart:'MSPointerDown',touchend:'MSPointerUp'}}document.addEventListener(t.touchstart,function(t){var n=t.touches||[t],r;for(var i=0,s=n.length;i<s;i++){r=n[i];e[r.identifier||r.pointerId]=r}},false);(window)";
                         view.loadUrl(javascript);
                     }
                 });
